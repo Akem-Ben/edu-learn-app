c2358159430b16642aab3e115b4e667d6a2f039a
Akem-Ben <akemini.ndaobong@gmail.com>
2024-12-17 20:00:43 +0100

    Home page completion

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,28 @@
 import Button from '@/components/Button';
-import React from 'react';
+import React, { useRef } from 'react';
 import { Text, View } from 'react-native';
 import Animated, { FadeInDown } from 'react-native-reanimated';
+import { router } from 'expo-router';
+import LottieView from 'lottie-react-native';
 
  const Welcome = () => {
+
+    const animation = useRef<LottieView>(null)
+
     return (
       <View className='bg-white gap-4 p-4 flex-1 w-full text-center justify-center items-center'>
+
+          <Animated.View className='w-full'
+        entering={FadeInDown.duration(300).springify()}>
+           <LottieView
+           ref={animation} 
+           source={require('../assets/animation/learner.json')}
+           autoPlay={true}
+           loop={true}
+           style={{width: '100%', height: 400}}
+           />
+        </Animated.View>
+
         <Animated.View className='w-full'
         entering={FadeInDown.duration(300).delay(200).springify()}>
             <Text className='text-5xl text-center leading-[3.5rem]'
@@ -24,7 +41,7 @@ import Animated, { FadeInDown } from 'react-native-reanimated';
 
         <Animated.View className='w-full justify-center items-center mt-8'
         entering={FadeInDown.duration(300).delay(600).springify()}>
-            <Button title={'Click Me'} />
+            <Button title={'Get Started'} action={() => router.push('/(tabs)')}/>
         </Animated.View>
 
       </View>
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, Pressable } from 'react-native';
+import { Text, Pressable, View } from 'react-native';
 
 interface ButtonProps {
     title: string;
