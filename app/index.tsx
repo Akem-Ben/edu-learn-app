a2b804a2ec97819190ac3fe8f15c7758ea76a32d
Akem-Ben <akemini.ndaobong@gmail.com>
2024-12-17 13:31:09 +0100

    Hello world

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+ const Welcome = () => {
+    return (
+      <View className='flex-1 justify-center items-center bg-orange-400'>
+        <Text className='text-2xl'> Hello World </Text>
+      </View>
+    )
+}
+
+
+export default Welcome
